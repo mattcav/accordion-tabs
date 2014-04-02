70dfe0d036f59ba79b2645da8d3bc7d520036d44
mattcav <matteo@mced.it>
2014-04-02 19:21:01 +0200

    fist approach on css-only

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,15 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+        }
+      }
+    },
+
     sass: {
-      options: {
-        includePaths: ['bower_components/foundation/scss']
-      },
       dist: {
         options: {
           outputStyle: 'compressed'
@@ -16,19 +21,36 @@ module.exports = function(grunt) {
       }
     },
 
+    autoprefixer: {
+      single_file: {
+        options: {
+          browsers: ['last 2 version', 'ie 8', 'ie 7']
+        },
+        src: 'css/app.css',
+        dest: 'css/app.css'
+      },
+    },
+
     watch: {
       grunt: { files: ['Gruntfile.js'] },
 
       sass: {
         files: 'scss/**/*.scss',
-        tasks: ['sass']
+        tasks: ['sass', 'autoprefixer']
+      },
+
+      options: {
+        livereload: true
       }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-autoprefixer');
 
-  grunt.registerTask('build', ['sass']);
+
+  grunt.registerTask('build', ['connect', 'sass', 'autoprefixer']);
   grunt.registerTask('default', ['build','watch']);
 }
